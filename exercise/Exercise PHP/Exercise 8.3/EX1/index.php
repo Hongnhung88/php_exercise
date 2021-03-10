8627216c5e40640120dabdc078ca5a9a5cec1928
Hongnhung88 <daohongnhungtd88@gmail.com>
2021-03-10 15:59:57 +0700

    update exercise Php

--- exercise/Exercise PHP/Exercise 8.3/EX1/index.php
+++ exercise/Exercise PHP/Exercise 8.3/EX1/index.php
@@ -1,7 +1,12 @@
 <?php
   $a = 2;
   $b = 2;
-  echo 'a=2'."<br/>";
-  echo 'b=2'."<br/>";
-  echo 'tong='.$result= ($a + $b)* 3;
+  $tong = $a + $b ;
+  $result= ($a + $b)* 3;
+  echo 'a = 2'."<br/>";
+  echo 'b = 2'."<br/>";
+  if ($a = $b) {
+  	echo 'tổng = '.$result;
+  }else echo "tổng = ".$tong;
+  
 ?>
